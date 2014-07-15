bff7f2e1c85ba213daba3e7aab2e2259cbc6d609
Tim Telcik <T.Telcik@aims.gov.au>
2014-07-15 23:40:21 +0800

    Inital commit after migration from Ant Liferay Plugin SDK project to Lifereay Maven SDK project.

--- README.md
+++ README.md
@@ -1,4 +1,143 @@
-liferay-request-dumper
-======================
+# Liferay Request Dumper
 
-Liferay Portal servlet filter which logs all web requests and responses messages via the Liferay Portal 6.x logging framework.
+*liferay-request-dumper*
+
+This project provides a Liferay Portal servlet filter that logs all web requests and responses messages via the Liferay Portal logging framework.
+
+NOTE: The servlet filter is only compatible with Servlet API 3.0+
+
+
+## Supported Products
+
+### GitHub Project Master
+
+* Equivalent to branch 6.1.x
+
+### GitHub Project Branch 6.1.x
+
+* Liferay Portal 6.1 CE : 6.1 CE GA2, GA3 (6.1.1+)
+* Liferay Portal 6.1 CE : 6.1 EE GA2, GA3 (6.1.20+)
+
+
+## Downloads
+
+The latest release is available from [SourceForge - Liferay Request Dumper](https://sourceforge.net/projects/permeance-apps/files/liferay-request-dumper/ "Liferay Request Dumper").
+
+
+## Usage
+
+Step 1. Configure Liferay Portal logging for Liferay Request Dumper.
+
+Create or update log4j settings file. 
+
+NOTE: For a Liferay Portal + Tomcat bundle the log4j settings file is located at 
+"LIFERAY_HOME/tomcat-xxx/lib/ext/META-INF/portal-log4j-ext.xml".
+
+Sample “portal-log4j-ext.xml”
+
+<?xml version="1.0"?>
+<!DOCTYPE log4j:configuration SYSTEM "log4j.dtd">
+
+<log4j:configuration xmlns:log4j="http://jakarta.apache.org/log4j/" debug="true">
+
+        <appender name="CONSOLE" class="org.apache.log4j.ConsoleAppender">
+                <layout class="org.apache.log4j.EnhancedPatternLayout">
+                        <param name="ConversionPattern" value="%d{ISO8601}{Australia/Perth} %-5p [%t][%c{1}:%L] %m%n" />
+                </layout>
+        </appender>
+
+        <appender name="FILE" class="org.apache.log4j.rolling.RollingFileAppender">
+                <rollingPolicy class="org.apache.log4j.rolling.TimeBasedRollingPolicy">
+                        <param name="FileNamePattern" value="@liferay.home@/logs/liferay@spi.id@.%d{yyyy-MM-dd}.log" />
+                </rollingPolicy>
+
+                <layout class="org.apache.log4j.EnhancedPatternLayout">
+                        <param name="ConversionPattern" value="%d{ISO8601}{Australia/Perth} %-5p [%t][%c{1}:%L] %m%n" />
+                </layout>
+        </appender>
+
+        <category name="au.com.permeance.liferay.portal.servlet.filters.request.RequestDumperFilter">
+                <priority value="ALL" />
+        </category>
+
+        <root>
+                <priority value="INFO" />
+                <appender-ref ref="CONSOLE" />
+                <appender-ref ref="FILE" />
+        </root>
+
+</log4j:configuration>
+
+Step 2. Enable or disable Liferay Request Dumper in portal properties.
+
+Edit file “LIFERAY_HOME/portal-ext.properties”.
+
+-# Enable RequestDumperFilter
+au.com.permeance.liferay.portal.servlet.filters.request.RequestDumperFilter=true
+
+-# Disable RequestDumperFilter
+au.com.permeance.liferay.portal.servlet.filters.request.RequestDumperFilter=false
+
+## Building
+
+Step 1. Checkout source from GitHub project
+
+Step 1.1. Checkout master from GitHub project
+
+    NOTE: GitHub master and branch 6.1.x should always be the same.
+
+    $ md work
+    $ cd work
+    $ md master
+    $ git clone https://github.com/permeance/liferay-request-dumper
+    Cloning into 'liferay-request-dumper'...
+    remote: Counting objects: 518, done.
+    remote: Compressing objects: 100% (223/223), done.
+    remote: Total 518 (delta 173), reused 502 (delta 157)
+    Receiving objects: 100% (518/518), 622.65 KiB | 273.00 KiB/s, done.
+    Resolving deltas: 100% (173/173), done.
+    Checking connectivity... done
+
+Step 1.2. Checkout branch 6.1.x from GitHub project
+
+    NOTE: This sample shows checkout for branch 6.1.x. 
+
+    % md work
+    % cd work
+    % md -p liferay-request-dumper/branches/6.1.x
+    % cd liferay-request-dumper/branches/6.1.x
+    % git clone https://github.com/permeance/liferay-request-dumper
+    Cloning into 'liferay-request-dumper'...
+    remote: Counting objects: 475, done.
+    remote: Compressing objects: 100% (199/199), done.
+    remote: Total 475 (delta 151), reused 470 (delta 146)
+    Receiving objects: 100% (475/475), 618.21 KiB | 161.00 KiB/s, done.
+    Resolving deltas: 100% (151/151), done.
+    Checking connectivity... done
+    % cd liferay-request-dumper
+    % git branch 6.1.x
+    % git checkout 6.1.x
+    Switched to branch '6.1.x'
+    % git status
+    # On branch 6.1.x
+    nothing to commit, working directory clean
+
+Step 2. Build and package
+
+    % mvn -U clean package
+
+This will build "liferay-request-dumper-A.B.C.war" in the targets tolder.
+
+
+## Installation
+
+### Liferay Portal + Apache Tomcat Bundle
+
+eg.
+
+Deploy "liferay-request-dumper-A.B.C.war" to "LIFERAY_HOME/deploy" folder.
+
+
+## Project Team
+
+* Tim Telcik - tim.telcik@permeance.com.au
---
+++ src/main/java/au/com/permeance/liferay/portal/servlet/filters/request/RequestDumperFilter.java
@@ -0,0 +1,324 @@
+/**
+ * Copyright (C) 2014 Permeance Technologies
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+package au.com.permeance.liferay.portal.servlet.filters.request;
+
+import au.com.permeance.liferay.portal.kernel.servlet.BasePortalFilter;
+
+import com.liferay.portal.kernel.util.HttpUtil;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Enumeration;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Request Dumper Filter.
+ * 
+ * This Request Dumper Filter is based on the Apache Tomcat 7.x Request Dumper Filter 
+ * and implemented using the Liferay Portal 6.1.x Servlet Filter framework.
+ * 
+ * This Request Dumper Filter was developed to work around classloading and logging configuration
+ * issues with using the default Apache Tomcat 7.x Request Dumper Filter with Liferay Portal 6.1.x. 
+ * 
+ * This Request Dumper Filter logs messages via the Liferay Portal 6.1.x logging framework.
+ * 
+ * @author Craig R. McClanahan
+ * @author Tim Telcik <tim.telcik@permeance.com.au>
+ * 
+ * @see http://tomcat.apache.org/tomcat-7.0-doc/config/filter.html#Request_Dumper_Filter
+ * @see https://tomcat.apache.org/tomcat-7.0-doc/api/org/apache/catalina/filters/RequestDumperFilter.html
+ * 
+ * @see http://grepcode.com/file/repo1.maven.org/maven2/org.apache.tomcat.embed/tomcat-embed-core/7.0.0/org/apache/catalina/filters/RequestDumperFilter.java
+ * @see http://www.liferay.com/documentation/liferay-portal/6.1/development/-/ai/other-hooks
+ * @see https://github.com/liferay/liferay-plugins/tree/master/hooks/sample-servlet-filter-hook
+ * 
+ * @see https://tomcat.apache.org/tomcat-7.0-doc/api/org/apache/catalina/filters/RequestDumperFilter.html
+ * @see BasePortalFilter
+ */
+public class RequestDumperFilter extends au.com.permeance.liferay.portal.kernel.servlet.BasePortalFilter {
+
+	public static final String SKIP_FILTER = RequestDumperFilter.class.getName() + "SKIP_FILTER";
+	
+    private static final String NON_HTTP_REQ_MSG = "Not available. Non-HTTP request.";
+    
+	private static final String NON_HTTP_RES_MSG = "Not available. Non-HTTP response.";
+
+	private static final ThreadLocal<Timestamp> timestamp = new ThreadLocal<Timestamp>() {
+		@Override
+		protected Timestamp initialValue() {
+			return new Timestamp();
+		}
+	};
+	
+	
+	public RequestDumperFilter() {
+		super();
+	}
+
+	
+	@Override
+	public boolean isFilterEnabled(
+		HttpServletRequest request, HttpServletResponse response) {
+
+		boolean filterEnabled = false;
+		
+		if (isAlreadyFiltered(request)) {
+			getLog().info("already filtered; skip");
+			filterEnabled = false;
+		}
+		else {
+			getLog().info("not yet not filtered; continue");
+			filterEnabled = true;
+		}
+		
+		getLog().info("filterEnabled: " + filterEnabled);
+		return filterEnabled;
+	}
+	
+	
+	protected boolean isAlreadyFiltered(HttpServletRequest request) {
+		if (request.getAttribute(SKIP_FILTER) != null) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+
+	/**
+	 * This processFilter method is essentially a clone 
+	 * of Tomcat's RequestDumperFilter#doFilter(ServletRequest,ServletResponse,FilterChain)
+	 * merged with Liferay Portal's hook filter mechanism.
+	 * 
+	 * @see http://grepcode.com/file/repo1.maven.org/maven2/org.apache.tomcat.embed/tomcat-embed-core/7.0.0/org/apache/catalina/filters/RequestDumperFilter.java
+	 */
+	@Override
+	protected void processFilter(
+			HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+		throws Exception {
+
+		request.setAttribute(SKIP_FILTER, Boolean.TRUE);
+		
+        HttpServletRequest hRequest = null;
+        HttpServletResponse hResponse = null;
+        
+        if (request instanceof HttpServletRequest) {
+            hRequest = (HttpServletRequest) request;
+        }
+        if (response instanceof HttpServletResponse) {
+            hResponse = (HttpServletResponse) response;
+        }		
+
+        // Log pre-service information
+        doLog("START TIME        ", getTimestamp());
+
+		String completeURL = HttpUtil.getCompleteURL(request);
+		
+		if (getLog().isInfoEnabled()) {
+			getLog().info("Begin filter for request dumper at URL " + completeURL);
+		}
+        
+        if (hRequest == null) {
+            doLog("        requestURI", NON_HTTP_REQ_MSG);
+            doLog("          authType", NON_HTTP_REQ_MSG);
+        } else {
+            doLog("        requestURI", hRequest.getRequestURI());
+            doLog("          authType", hRequest.getAuthType());
+        }
+        
+        doLog(" characterEncoding", request.getCharacterEncoding());
+        doLog("     contentLength",
+                Integer.valueOf(request.getContentLength()).toString());
+        doLog("       contentType", request.getContentType());
+        
+        if (hRequest == null) {
+            doLog("       contextPath", NON_HTTP_REQ_MSG);
+            doLog("            cookie", NON_HTTP_REQ_MSG);
+            doLog("            header", NON_HTTP_REQ_MSG);
+        } else {
+            doLog("       contextPath", hRequest.getContextPath());
+            Cookie cookies[] = hRequest.getCookies();
+            if (cookies != null) {
+                for (int i = 0; i < cookies.length; i++)
+                    doLog("            cookie", cookies[i].getName() +
+                            "=" + cookies[i].getValue());
+            }
+            Enumeration<String> hnames = hRequest.getHeaderNames();
+            while (hnames.hasMoreElements()) {
+                String hname = hnames.nextElement();
+                Enumeration<String> hvalues = hRequest.getHeaders(hname);
+                while (hvalues.hasMoreElements()) {
+                    String hvalue = hvalues.nextElement();
+                    doLog("            header", hname + "=" + hvalue);
+                }
+            }
+        }
+        
+        doLog("            locale", request.getLocale().toString());
+        
+        if (hRequest == null) {
+            doLog("            method", NON_HTTP_REQ_MSG);
+        } else {
+            doLog("            method", hRequest.getMethod());
+        }
+        
+        Enumeration<String> pnames = request.getParameterNames();
+        while (pnames.hasMoreElements()) {
+            String pname = pnames.nextElement();
+            String pvalues[] = request.getParameterValues(pname);
+            StringBuilder result = new StringBuilder(pname);
+            result.append('=');
+            for (int i = 0; i < pvalues.length; i++) {
+                if (i > 0)
+                    result.append(", ");
+                result.append(pvalues[i]);
+            }
+            doLog("         parameter", result.toString());
+        }
+        
+        if (hRequest == null) {
+            doLog("          pathInfo", NON_HTTP_REQ_MSG);
+        } else {
+            doLog("          pathInfo", hRequest.getPathInfo());
+        }
+        
+        doLog("          protocol", request.getProtocol());
+        
+        if (hRequest == null) {
+            doLog("       queryString", NON_HTTP_REQ_MSG);
+        } else {
+            doLog("       queryString", hRequest.getQueryString());
+        }
+        
+        doLog("        remoteAddr", request.getRemoteAddr());
+        doLog("        remoteHost", request.getRemoteHost());
+        
+        if (hRequest == null) {
+            doLog("        remoteUser", NON_HTTP_REQ_MSG);
+            doLog("requestedSessionId", NON_HTTP_REQ_MSG);
+        } else {
+            doLog("        remoteUser", hRequest.getRemoteUser());
+            doLog("requestedSessionId", hRequest.getRequestedSessionId());
+        }
+        
+        doLog("            scheme", request.getScheme());
+        doLog("        serverName", request.getServerName());
+        doLog("        serverPort",
+                Integer.valueOf(request.getServerPort()).toString());
+        
+        if (hRequest == null) {
+            doLog("       servletPath", NON_HTTP_REQ_MSG);
+        } else {
+            doLog("       servletPath", hRequest.getServletPath());
+        }
+        
+        doLog("          isSecure",
+                Boolean.valueOf(request.isSecure()).toString());
+        doLog("------------------",
+                "--------------------------------------------");
+
+        // Perform the request
+		processFilter(RequestDumperFilter.class, hRequest, hResponse, filterChain);
+
+        // Log post-service information
+        doLog("------------------",
+                "--------------------------------------------");
+        if (hRequest == null) {
+            doLog("          authType", NON_HTTP_REQ_MSG);
+        } else {
+            doLog("          authType", hRequest.getAuthType());
+        }
+        
+        doLog("       contentType", response.getContentType());
+        
+        if (hResponse == null) {
+            doLog("            header", NON_HTTP_RES_MSG);
+        } else {
+            Iterable<String> rhnames = hResponse.getHeaderNames();
+            for (String rhname : rhnames) {
+                Iterable<String> rhvalues = hResponse.getHeaders(rhname);
+                for (String rhvalue : rhvalues)
+                    doLog("            header", rhname + "=" + rhvalue);
+            }
+        }
+
+        if (hRequest == null) {
+            doLog("        remoteUser", NON_HTTP_REQ_MSG);
+        } else {
+            doLog("        remoteUser", hRequest.getRemoteUser());
+        }
+        
+        if (hResponse == null) {
+            doLog("        remoteUser", NON_HTTP_RES_MSG);
+        } else {
+            doLog("            status",
+                    Integer.valueOf(hResponse.getStatus()).toString());
+        }
+
+        doLog("END TIME          ", getTimestamp());
+        doLog("==================",
+                "============================================");
+        
+		if (getLog().isInfoEnabled()) {
+			getLog().info("End filter for request dumper at URL " + completeURL);
+		} 
+	}
+	
+	
+    private void doLog(String attribute, String value) {
+        StringBuilder sb = new StringBuilder(80);
+        sb.append(Thread.currentThread().getName());
+        sb.append(' ');
+        sb.append(attribute);
+        sb.append('=');
+        sb.append(value);
+        getLog().info(sb.toString());
+        // _log.info(sb.toString());
+    }
+
+    
+    private String getTimestamp() {
+        Timestamp ts = timestamp.get();
+        long currentTime = System.currentTimeMillis();
+        
+        if ((ts.date.getTime() + 999) < currentTime) {
+            ts.date.setTime(currentTime - (currentTime % 1000));
+            ts.update();
+        }
+        return ts.dateString;
+    }
+	
+	
+    private static final class Timestamp {
+        private Date date = new Date(0);
+        private SimpleDateFormat format =
+            new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
+        private String dateString = format.format(date);
+        private void update() {
+            dateString = format.format(date);
+        }
+    }	
+
+	
+	// private static Log _log = LogFactoryUtil.getLog(RequestDumperFilter.class);
+
+}
